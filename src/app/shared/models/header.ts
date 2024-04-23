475999bcbb23badf11019b41e774a3820dca3378
ahmedsalama2030 <ahmed.salama.ali.ramadan@gmail.com>
2024-04-24 00:51:15 +0200

    Header

--- src/app/shared/models/header.ts
+++ src/app/shared/models/header.ts
@@ -1,28 +1,31 @@
 export interface HeaderList {
-  success: boolean
-  data: Header[]
-  message: string
-  statusCode: number
+  success: boolean;
+  data: Header[];
+  message: string;
+  statusCode: number;
 }
 
 export interface Header {
-  sort: number
-  text: string
-  isShowHeader: boolean
-  parentId: any
-  isExternal: boolean
-  linkExternal: string
-  childs: Child[]
-  id: number
+  sort: number;
+  text: string;
+  textAr: string;
+  textEn: string;
+
+  isShowHeader: boolean;
+  parentId: any;
+  isExternal: boolean;
+  linkExternal: string;
+  childs: Child[];
+  id: number;
 }
 
 export interface Child {
-  sort: number
-  text: string
-  isShowHeader: boolean
-  parentId: any
-  isExternal: boolean
-  linkExternal?: string
-  childs: any[]
-  id: number
+  sort: number;
+  text: string;
+  isShowHeader: boolean;
+  parentId: any;
+  isExternal: boolean;
+  linkExternal?: string;
+  childs: any[];
+  id: number;
 }
